6ff0f21dfd72d18c9bd0acf0b15a762cafa75abb
HiepThuongTitus <thuongthithuhiep2452000123@gmail.com>
2021-09-25 20:00:50 +0700

    chap7

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "static_pages/help"
   get "/home", to: "static_pages#home"
   get "/help", to: "static_pages#help"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
